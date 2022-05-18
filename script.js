d69525f7505721e0bb98e8640b160de24ec0f9ff
Darlan Carvalho <darlan171201@gmail.com>
2022-05-18 20:40:43 -0300

    Desafio básico de Javascript da DevQuest

---
+++ script.js
@@ -0,0 +1,25 @@
+
+somar( 10, 5)
+subtrair(10, 5)
+multiplicar( 10, 5)
+dividir( 10, 5)
+
+function somar(x, y){
+    let soma = x + y
+    console.log("A soma de " + x + " e " + y + " é " + soma)
+}
+
+function subtrair(x , y){
+    let subtracao = x - y
+    console.log("A subtração de " + x + " e " + y + " é " + subtracao)
+}
+
+function multiplicar(x , y){
+    let multiplicacao = x * y
+    console.log("A multiplicação de " + x + " e " + y + " é " + multiplicacao)
+}
+
+function dividir(x , y){
+    let divisao = x / y
+    console.log("A divisão de " + x + " e " + y + " é " + divisao)
+}
